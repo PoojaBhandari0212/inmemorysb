836629381b89ff5f75f3d7ac7fc73c2569768f3f
Chetan <bhandaripooja0212@gmail.com>
2025-06-14 19:34:39 +0530

    initial commits

---
+++ src/main/java/com/inmemory/inmemorysb/InmemorysbApplication.java
@@ -0,0 +1,13 @@
+package com.inmemory.inmemorysb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InmemorysbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InmemorysbApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/inmemory/inmemorysb/InmemorysbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.inmemory.inmemorysb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InmemorysbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
